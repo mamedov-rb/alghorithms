7d8f50b2d94f0ff32289b9f65e3b10bd3d51b852
Rustam Mamedov <alwaysontheroad.rb@gmial.com>
2022-05-22 14:24:03 +0300

    some renaming

--- src/main/java/ArraysPart.java
+++ src/main/java/ArraysPart.java
@@ -1,29 +1,30 @@
 public class ArraysPart {
 
-    /* 001. Return new length as it was removed all instances of element (unsorted array) */
-    public static int removeElementReturnNewLength(int[] array, int el) {
+    /* 001. Return new length as it was removed all duplicates (sorted array) */
+    public static int removeDuplicatesReturningNewLength(int[] array) {
         if (array == null) return 0;
+        if (array.length == 1) return 1;
 
         int i = 0;
-        int L = 0;
-        while (i < array.length) {
-            if (array[i++] != el) {
-                L++;
+        int j = 1;
+        int L = 1;
+        while (j < array.length) {
+            if (array[i++] != array[j++]) {
+               L++;
             }
         }
         return L;
     }
 
-    /* 002. Return new length as it was removed all duplicates (sorted array) */
-    public static int removeDuplicatesReturningNewLength(int[] array) {
+    /* 002. Return new length as it was removed all instances of element (unsorted array) */
+    public static int removeElementReturnNewLength(int[] array, int el) {
         if (array == null) return 0;
-        if (array.length < 2) return array.length;
+        if (array.length == 1) return 1;
 
         int i = 0;
-        int j = 1;
         int L = 0;
-        while (j < array.length) {
-            if (array[i++] != array[j++]) {
+        while (i < array.length) {
+            if (array[i++] != el) {
                 L++;
             }
         }
--- src/test/groovy/ArraysPartTest.groovy
+++ src/test/groovy/ArraysPartTest.groovy
@@ -2,23 +2,23 @@ import spock.lang.Specification
 
 class ArraysPartTest extends Specification {
 
+    def "return new length as it was removed all duplicates"() {
+        given:
+        def array = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5] as int[]
+
+        when:
+        def newLength = ArraysPart.removeDuplicatesReturningNewLength(array)
+
+        then:
+        newLength == 5
+    }
+
     def "return new length as it was removed all instances of element"() {
         given:
         def array = [1, 1, 2, 3, 1] as int[]
 
         when:
         def newLength = ArraysPart.removeElementReturnNewLength(array, 2)
-
-        then:
-        newLength == 4
-    }
-
-    def "return new length as it was removed all duplicates"() {
-        given:
-        def array = [1, 2, 3, 4, 4, 5] as int[]
-
-        when:
-        def newLength = ArraysPart.removeDuplicatesReturningNewLength(array)
 
         then:
         newLength == 4
